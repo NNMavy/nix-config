bfe1673409c4ad7da3c8b5a5207544f6d7e5c920
Rene Koens <mavy@ninjanerd.eu>
2024-04-07 23:06:13 +0200

    chore: someone fails and it isn't me
    Commit generated by whatthecommit.com
    
    Signed-off-by: Rene Koens <mavy@ninjanerd.eu>

--- hosts/_modules/common/nix.nix
+++ hosts/_modules/common/nix.nix
@@ -11,12 +11,12 @@
       warn-dirty = false;
 
       substituters = [
-        "https://szinn.cachix.org"
+        "https://mavy.cachix.org"
         "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
-        "szinn.cachix.org-1:9gbZrHCd1BYMUuMCinvG2fTu98Jubp8y8tLE3jipABM="
+        "mavy.cachix.org-1:fsS3qUaevKZDag/2fMrGAB9wEBd7pkrKihM071JuxLk="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
