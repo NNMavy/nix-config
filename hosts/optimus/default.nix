4d5f34019e905008e0a6b7c2141a5cfdba60adc9
René Koens <mavy@ninjanerd.eu>
2024-05-18 20:39:12 +0200

    feat(optimus): Start with new laptop deploy
    Signed-off-by: René Koens <mavy@ninjanerd.eu>

---
+++ hosts/optimus/default.nix
@@ -0,0 +1,91 @@
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}: let
+  ifGroupsExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
+in {
+  imports = [
+    ./hardware-configuration.nix
+    ./plasma.nix
+    inputs.nixos-hardware.nixosModules.common-cpu-intel
+  ];
+
+  networking = {
+    hostName = "optimus";
+    firewall.enable = false;
+    networkmanager.enable = true;
+  };
+
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+    ];
+  };
+
+  users.users.mavy = {
+    uid = 1000;
+    description = "Rene Koens";
+    name = "mavy";
+    home = "/home/mavy";
+    group = "mavy";
+    shell = pkgs.fish;
+    packages = [pkgs.home-manager];
+    openssh.authorizedKeys.keys = [(builtins.readFile ../../homes/mavy/config/ssh/ssh.pub)];
+    hashedPasswordFile = config.sops.secrets.mavy-password.path;
+    isNormalUser = true;
+    extraGroups =
+      ["wheel"]
+      ++ ifGroupsExist [
+        "networkmanager"
+        "samba-users"
+      ];
+  };
+  users.groups.mavy = {
+    gid = 1000;
+  };
+
+  sops.secrets.mavy-password = {
+    sopsFile = ../../homes/mavy/hosts/optimus/secrets.sops.yaml;
+    neededForUsers = true;
+  };
+
+  system.activationScripts.postActivation.text = ''
+    # Must match what is in /etc/shells
+    chsh -s /run/current-system/sw/bin/fish mavy
+  '';
+
+  modules = {
+    applications = {
+      one-password.enable = true;
+    };
+    services = {
+      openssh.enable = true;
+    };
+
+    users = {
+      groups = {
+        homelab = {
+          gid = 568;
+          members = ["mavy"];
+        };
+      };
+      additionalUsers = {
+        homelab = {
+          uid = 568;
+          group = "homelab";
+          isNormalUser = false;
+        };
+      };
+    };
+  };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+  };
+}
