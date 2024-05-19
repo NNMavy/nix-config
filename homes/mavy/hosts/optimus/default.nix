c22d9367d96349561abf037b79d6bfd7be5d1669
René Koens <mavy@ninjanerd.eu>
2024-05-19 09:11:06 +0200

    feat(optimus): More progress on optimus
    Signed-off-by: René Koens <mavy@ninjanerd.eu>

---
+++ homes/mavy/hosts/optimus/default.nix
@@ -0,0 +1,30 @@
+{
+  inputs,
+  pkgs,
+  config,
+  ...
+}: {
+  imports = [
+  ];
+
+  home = {
+    username = "mavy";
+    homeDirectory = "/home/mavy";
+    sessionVariables = {
+      SOPS_AGE_KEY_FILE = "${config.xdg.configHome}/sops/age/keys.txt";
+    };
+  };
+
+  modules = {
+    applications.vmware-horizon.enable = true;
+    editors = {
+      neovim.enable = true;
+      # vscode.server-enable = true;
+    };
+    shell = {
+      tmux.enable = true;
+      alacritty.enable = true;
+      starship.enable = true;
+    };
+  };
+}
