622ed5997d300223654a9397e6221a0ccb25c12d
René Koens <mavy@ninjanerd.eu>
2024-05-26 12:51:11 +0200

    Bumblebee (#30)
    Signed-off-by: Rene Koens <mavy@ninjanerd.eu>

---
+++ nixos/hosts/bumblebee/default.nix
@@ -0,0 +1,56 @@
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  config = {
+
+    mySystem = {
+      purpose = "Homelab";
+      persistentFolder = "/persist";
+      system.impermanence.enable = true;
+      services.openssh.enable = true;
+      security.wheelNeedsSudoPassword = false;
+    };
+
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    networking.hostName = "bumblebee"; # Define your hostname.
+    networking.hostId = "bce6d044";
+    networking.useDHCP = lib.mkDefault true;
+
+    fileSystems."/" =
+      {
+        device = "rpool/local/root";
+        fsType = "zfs";
+      };
+
+    fileSystems."/nix" =
+      {
+        device = "rpool/local/nix";
+        fsType = "zfs";
+      };
+
+    fileSystems."/persist" =
+      {
+        device = "rpool/safe/persist";
+        fsType = "zfs";
+        neededForBoot = true; # for impermanence
+      };
+
+    fileSystems."/boot" =
+      {
+        device = "/dev/disk/by-uuid/E6EC-784B";
+        fsType = "vfat";
+        options = [ "fmask=0022" "dmask=0022" ];
+      };
+
+    swapDevices =
+      [ { device = "/dev/disk/by-uuid/493a7dc4-c85f-4719-8100-756640d5a73a"; }
+      ];
+
+  };
+}
