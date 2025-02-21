afbb2b838b1a4baf4e3dbbcebc65dfeff2fa9e3b
Rene Koens <mavy@ninjanerd.eu>
2025-02-21 15:50:22 +0100

    feat: Enable hibernate and other tweaks
    Signed-off-by: Rene Koens <mavy@ninjanerd.eu>

--- nixos/profiles/hw-framework13-x86.nix
+++ nixos/profiles/hw-framework13-x86.nix
@@ -26,6 +26,8 @@ with lib;
       grub.memtest86.enable = true;
 
     };
+
+    kernelPackages = pkgs.linuxPackages_6_12; # Latest LTS kernel
   };
 
   hardware = {
@@ -39,9 +41,44 @@ with lib;
 
     bluetooth.enable = true; # enables support for Bluetooth
     bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+    #framework.enableKmod = true; # enables the kernel module for the Framework Laptop
   };
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   services.xserver.videoDrivers = [ "intel" ];
+
+  # suspend-then-hibernate
+  systemd.sleep.extraConfig = ''
+    HibernateDelaySec=30m
+    SuspendState=mem
+    AllowSuspend=yes
+    AllowHibernation=yes
+    AllowHybridSleep=yes
+    AllowSuspendThenHibernate=yes
+  '';
+
+  #laptop Optmization
+  powerManagement.enable = true;
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
+
+  #     CPU_MIN_PERF_ON_AC = 0;
+  #     CPU_MAX_PERF_ON_AC = 100;
+  #     CPU_MIN_PERF_ON_BAT = 0;
+  #     CPU_MAX_PERF_ON_BAT = 40;
+
+  #     #Optional helps save long term battery health
+  #     START_CHARGE_THRESH_BAT0 = 20; # 40 and bellow it starts to charge
+  #     STOP_CHARGE_THRESH_BAT0 = 98; # 80 and above it stops charging
+
+  #   };
+  # };
+
 }
