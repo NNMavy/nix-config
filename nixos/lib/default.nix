9ad5e3fd163061bc1e7e2d0e035b667062a75170
Rene Koens <mavy@ninjanerd.eu>
2025-03-25 06:53:05 +0100

    chore: The same thing we do every night, Pinky - try to take over the world!
    Commit generated by whatthecommit.com
    
    Signed-off-by: Rene Koens <mavy@ninjanerd.eu>

--- nixos/lib/default.nix
+++ nixos/lib/default.nix
@@ -120,9 +120,9 @@ rec {
   mapToGhaRunner =
     system:
     if system == "x86_64-linux" then
-      "self-hosted"
+      "ubuntu-latest"
     else if system == "aarch64-linux" then
-      "self-hosted"
+      "ubuntu-latest"
     else
       system;
 }
