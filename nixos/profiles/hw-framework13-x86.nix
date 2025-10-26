3b4b26676f237e1595f6e54cc8827fdfdecba407
NNMavy <3441334+NNMavy@users.noreply.github.com>
2025-10-26 08:58:12 +0100

    chore: Enable tlp
    Signed-off-by: NNMavy <3441334+NNMavy@users.noreply.github.com>

--- nixos/profiles/hw-framework13-x86.nix
+++ nixos/profiles/hw-framework13-x86.nix
@@ -60,25 +60,20 @@ with lib;
 
   #laptop Optmization
   powerManagement.enable = true;
-  # services.tlp = {
-  #   enable = true;
-  #   settings = {
-  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
-  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  powerManagement.powertop.enable = true;
 
-  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
-  #     CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-
-  #     CPU_MIN_PERF_ON_AC = 0;
-  #     CPU_MAX_PERF_ON_AC = 100;
-  #     CPU_MIN_PERF_ON_BAT = 0;
-  #     CPU_MAX_PERF_ON_BAT = 40;
-
-  #     #Optional helps save long term battery health
-  #     START_CHARGE_THRESH_BAT0 = 20; # 40 and bellow it starts to charge
-  #     STOP_CHARGE_THRESH_BAT0 = 98; # 80 and above it stops charging
-
-  #   };
-  # };
+  services = {
+    power-profiles-daemon.enable = false;
+    tlp = {
+      enable = true;
+      settings = {
+        CPU_BOOST_ON_AC = 1;
+        CPU_BOOST_ON_BAT = 0;
+        CPU_SCALING_GOVERNOR_ON_AC = "performance";
+        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+        STOP_CHARGE_THRESH_BAT0 = 95;
+      };
+    };
+  };
 
 }
